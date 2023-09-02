0b5236ac30994756f0774a4dc9cbdeb2abc567b8
Joao Mendes <joaosilva0721@gmail.com>
2023-09-02 16:04:37 -0300

    excluindo segundo primeiro print

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print('Bem vindos ao NewWordGues aaaa')
+
+
+
+print('Bem vindos ao NewWordGues')
+
+
+palavras_dicas = {
+    'roteador': 'Dispositivo responsável por encaminhar pacotes de dados entre diferentes redes.',
+    'protocolo': 'Conjunto de regras que define como os dados são transmitidos e recebidos em uma rede.',   
+}
+
+
