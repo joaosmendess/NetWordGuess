2c17c9fe3ba19462ce7723df9fc89e0ec48bb4a3
Ana Maria <a.soares0@gmail.com>
2023-09-02 15:42:13 -0300

    adicionando palavras

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
 print('Bem vindos ao NewWordGues')
+
+
+palavras_dicas = {
+    'roteador': 'Dispositivo responsável por encaminhar pacotes de dados entre diferentes redes.',
+    'protocolo': 'Conjunto de regras que define como os dados são transmitidos e recebidos em uma rede.',   
+}
